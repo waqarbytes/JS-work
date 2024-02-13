736a071d2f1e27b2eb411335bdd68838059eda74
Mohammed Waqar <137026365+waqarbytes@users.noreply.github.com>
2024-02-13 19:49:45 +0530

    Add files via upload

---
+++ javascriptconditional.js
@@ -0,0 +1,17 @@
+console.log("Hello I am code runner");
+
+let age = 12
+let babyheight = 2
+
+console.log(age+babyheight)
+console.log(age-babyheight)
+console.log(age*babyheight)
+console.log(age/babyheight)
+console.log(age**babyheight)
+
+if((age+babyheight)> 18){
+    console.log("You can drive");
+}
+else{
+    console.log("you cannot drive");
+}
---
+++ variables.js
@@ -0,0 +1,47 @@
+console.log("Hey this is javascript")
+
+let a = 5;
+let b = 6;
+let c = "waqar";
+
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+
+{
+    // let a =88;
+    console.log(a)
+}
+console.log(a)
+// const  a1 = 6;
+
+let x = "Waqar";
+let y = "20"; 
+let z = "5.66";
+const u = true;
+let q = undefined;
+let s = null;
+
+console.log(x, y, z, u, q, s)
+console.log(typeof x,typeof y,typeof z,typeof u,typeof q,typeof s)
+// object key value
+let o = {
+    "name": " Mohd waqar",
+    "job code": 5600,
+    "is_handsome": true
+}
+ console.log(o);
+ o.salary = "100 crores";
+ console.log(o);
+ o.salary = "500crores";
+ console.log(o);
+ o.salary="600 crores";
+ console.log(o);
+ o.holidays="3 days";
+ console.log(o);
+  
+ alert("Warning if it open twice");
+ 
+
+
+
